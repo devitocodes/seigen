05e26cc5bf8db3736b6ca01fdc9593cc9ccb3b64
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2015-05-12 18:00:57 +0100

    Corrected ICs.

--- elastic-wave.py
+++ elastic-wave.py
@@ -15,9 +15,9 @@ except KeyError:
    
 parameters['form_compiler']['quadrature_degree'] = 4
 
-mesh = UnitSquareMesh(20, 20)
+mesh = UnitSquareMesh(20, 20, 'crossed')
 
-S = TensorFunctionSpace(mesh, "CG", 2)
+S = TensorFunctionSpace(mesh, "CG", 1)
 U = VectorFunctionSpace(mesh, "CG", 1)
 dimension = len(U.split())
 
@@ -33,7 +33,7 @@ u1 = Function(U)
 # Constants
 density = 1.0
 T = 5.0
-dt = 0.005
+dt = 0.01
 mu = 0.25
 l = 0.5
 
@@ -43,7 +43,7 @@ Vs = sqrt(mu/density) # S-wave velocity
 
 # Weak forms
 F_u = density*inner(w, (u - u0)/dt)*dx + inner(grad(w), s0)*dx
-F_s = inner(v, (s - s0)/dt)*dx - inner(v, l*(div(u0))*Identity(dimension))*dx - inner(v, mu*(grad(u0) + grad(u0).T))*dx
+F_s = inner(v, (s - s0)/dt)*dx - inner(v, l*(div(u0))*Identity(dimension))*dx - inner(v, mu*(grad(u1) + grad(u1).T))*dx
 
 problem_u = LinearVariationalProblem(lhs(F_u), rhs(F_u), u1)
 solver_u = LinearVariationalSolver(problem_u)
@@ -63,8 +63,14 @@ sic = Expression((('-b*sin(pi*x[0])*sin(pi*x[1])*sin(a*t)','0'),
                    ('0','b*sin(pi*x[0])*sin(pi*x[1])*sin(a*t)')), a=a, b=b, t=dt/2.0)
 s0.assign(sic)
 
+
+uexact = project(Expression(('a*cos(pi*x[0])*sin(pi*x[1])*cos(a*t)','-a*sin(pi*x[0])*cos(pi*x[1])*cos(a*t)'), a=a, t=5), U)
+sexact = project(Expression((('-b*sin(pi*x[0])*sin(pi*x[1])*sin(a*t)','0'),
+                   ('0','b*sin(pi*x[0])*sin(pi*x[1])*sin(a*t)')), a=a, b=b, t=5+dt/2.0), S)
+
 t = dt
-while t < T:
+temp = Function(U)
+while t <= T + 1e-12:
    print "t = %f" % t
    
    # Solve for the velocity vector
@@ -78,5 +84,11 @@ while t < T:
    # Move onto next timestep
    t += dt
    
-   output_u << u1
-   output_s << s1
+   #G = inner(w, u)*dx - inner(w, u1-uexact)*dx
+   #solve(lhs(G) == rhs(G), temp)
+
+   #output_u << u1
+   #output_s << s1
+   
+   print "|u-uexact| = %f" % errornorm(u1, uexact)
+   print "|s-sexact| = %f" % errornorm(s1, sexact)
