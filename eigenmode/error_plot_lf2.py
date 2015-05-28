b7f2db572d54e0c2452281c39d4a2251edb04aea
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2015-05-28 14:53:34 +0100

    Plot as PDF.

--- eigenmode/error_plot_lf2.py
+++ eigenmode/error_plot_lf2.py
@@ -3,7 +3,7 @@ import pylab
 degrees = range(1, 5)
 
 for d in degrees:
-   f = open("error_p%d_lf4.dat" % d, "r")
+   f = open("error_p%d_lf2.dat" % d, "r")
    f.readline() # Skip the header
    dx = []
    ux_error = []
@@ -13,18 +13,11 @@ for d in degrees:
       ux_error.append(float(s[2]))
 
    second_order = [1.0e-2*(1.0/4.0)**i for i in range(0, len(ux_error))]
-   third_order = [1.0e-2*(1.0/8.0)**i for i in range(0, len(ux_error))]
-   fourth_order = [1.0e-2*(1.0/16.0)**i for i in range(0, len(ux_error))]
    
    pylab.figure()
    pylab.loglog(dx, ux_error, 'g--o', label="Velocity")
    
-   if(d == 1):
-      pylab.loglog(dx, second_order, label="Second-order convergence")
-   elif(d == 2):
-      pylab.loglog(dx, third_order, label="Third-order convergence")
-   else:
-      pylab.loglog(dx, fourth_order, label="Fourth-order convergence")
+   pylab.loglog(dx, second_order, label="Second-order convergence")
    pylab.legend(loc='best')
    pylab.xlabel("Characteristic element length")
    pylab.ylabel("Error in L2 norm")
