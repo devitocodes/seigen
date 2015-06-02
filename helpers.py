468c9136ce8f0545eb653a3200a198e16ed0132a
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2015-06-02 15:08:34 +0100

    Added functions to compute P-wave and S-wave velocity in a new file called helpers.py.

---
+++ helpers.py
@@ -0,0 +1,7 @@
+def Vp(mu, l, density):
+   """ Return the P-wave velocity. """
+   return sqrt((l + 2*mu)/density)
+   
+def Vs(mu, density):
+   """ Return the S-wave velocity. """
+   return sqrt(mu/density)
