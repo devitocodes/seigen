517c85ec9742042376fb72cc72a7b36c6ed223e0
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2015-06-09 20:19:05 +0100

    Fixed explosive_source_lf4.py.

--- tests/explosive_source/explosive_source_lf4.py
+++ tests/explosive_source/explosive_source_lf4.py
@@ -6,7 +6,7 @@ from elastic_wave.helpers import *
 
 Lx = 300.0
 Ly = 100.0
-h = 0.1
+h = 2.5
 with timed_region('mesh generation'):
    mesh = RectangleMesh(int(Lx/h), int(Ly/h), Lx, Ly)
 elastic = ElasticLF4(mesh, "DG", 1, dimension=2)
@@ -22,21 +22,22 @@ print "S-wave velocity: %f" % Vs(elastic.mu, elastic.density)
 
 # Source
 a = 159.42
-elastic.source_expression = Expression("x[0] >= 44.5 && x[0] <= 45.5 && x[1] >= 148.5 && x[1] <= 149.5 ? (-1.0 + 2*a*pow(t - 0.3, 2))*exp(-a*pow(t - 0.3, 2)) : 0.0", a=a, t=0)
-HU = FunctionSpace(mesh, "DG", 4)
-elastic.source_function = Function(HU)
+elastic.source_expression = Expression((("x[0] >= 44.5 && x[0] <= 45.5 && x[1] >= 148.5 && x[1] <= 149.5 ? (-1.0 + 2*a*pow(t - 0.3, 2))*exp(-a*pow(t - 0.3, 2)) : 0.0", "0.0"),
+                                       ("0.0", "x[0] >= 44.5 && x[0] <= 45.5 && x[1] >= 148.5 && x[1] <= 149.5 ? (-1.0 + 2*a*pow(t - 0.3, 2))*exp(-a*pow(t - 0.3, 2)) : 0.0")), a=a, t=0)
+elastic.source_function = Function(elastic.S)
 elastic.source = elastic.source_expression
 
 # Absorption
-elastic.absorption_function = Function(elastic.U)
+F = FunctionSpace(mesh, "DG", 4)
+elastic.absorption_function = Function(F)
 elastic.absorption = Expression("x[0] <= 20 || x[0] >= 280 || x[1] <= 20.0 ? 1000 : 0")
 
 # Initial conditions
 uic = Expression(('0.0', '0.0'))
-elastic.u0.assign(Function(elastic.WU).interpolate(uic))
+elastic.u0.assign(Function(elastic.U).interpolate(uic))
 sic = Expression((('0','0'),
                   ('0','0')))
-elastic.s0.assign(Function(elastic.WS).interpolate(sic))
+elastic.s0.assign(Function(elastic.S).interpolate(sic))
 
 # Start the simulation
 T = 2.5 # If you just want to run 10 time-steps, use T = elastic.dt*10.0
