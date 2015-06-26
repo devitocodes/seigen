2d42a7d6aa58ffd929b80cd1e1058451d14b23fd
Michael Lange <michael.lange@imperial.ac.uk>
2015-06-26 13:22:58 +0100

    ESLF4: Create a class for ExplosiveSourceLF4

--- tests/explosive_source/explosive_source_lf4.py
+++ tests/explosive_source/explosive_source_lf4.py
@@ -4,41 +4,56 @@ from firedrake import *
 from elastic_wave.elastic import *
 from elastic_wave.helpers import *
 
-Lx = 300.0
-Ly = 150.0
-h = 2.5
-with timed_region('mesh generation'):
-   mesh = RectangleMesh(int(Lx/h), int(Ly/h), Lx, Ly)
-elastic = ElasticLF4(mesh, "DG", 2, dimension=2)
 
-# Constants
-elastic.density = 1.0
-elastic.dt = 0.001
-elastic.mu = 3600.0
-elastic.l = 3599.3664
+class ExplosiveSourceLF4():
 
-print "P-wave velocity: %f" % Vp(elastic.mu, elastic.l, elastic.density)
-print "S-wave velocity: %f" % Vs(elastic.mu, elastic.density)
+   def generate_mesh(self, Lx=300.0, Ly=150.0, h=2.5):
+      return RectangleMesh(int(Lx/h), int(Ly/h), Lx, Ly)
 
-# Source
-a = 159.42
-elastic.source_expression = Expression((("x[0] >= 44.5 && x[0] <= 45.5 && x[1] >= 148.5 && x[1] <= 149.5 ? (-1.0 + 2*a*pow(t - 0.3, 2))*exp(-a*pow(t - 0.3, 2)) : 0.0", "0.0"),
-                                       ("0.0", "x[0] >= 44.5 && x[0] <= 45.5 && x[1] >= 148.5 && x[1] <= 149.5 ? (-1.0 + 2*a*pow(t - 0.3, 2))*exp(-a*pow(t - 0.3, 2)) : 0.0")), a=a, t=0)
-elastic.source_function = Function(elastic.S)
-elastic.source = elastic.source_expression
+   def explosive_source_lf4(self, T=2.5, Lx=300.0, Ly=150.0, h=2.5,
+                            explicit=True, output=True):
 
-# Absorption
-F = FunctionSpace(mesh, "DG", 4)
-elastic.absorption_function = Function(F)
-elastic.absorption = Expression("x[0] <= 20 || x[0] >= 280 || x[1] <= 20.0 ? 1000 : 0")
+      with timed_region('mesh generation'):
+         mesh = self.generate_mesh()
+         self.elastic = ElasticLF4(mesh, "DG", 2, dimension=2,
+                                   explicit=explicit, output=output)
 
-# Initial conditions
-uic = Expression(('0.0', '0.0'))
-elastic.u0.assign(Function(elastic.U).interpolate(uic))
-sic = Expression((('0','0'),
+      # Constants
+      self.elastic.density = 1.0
+      self.elastic.dt = 0.001
+      self.elastic.mu = 3600.0
+      self.elastic.l = 3599.3664
+
+      print "P-wave velocity: %f" % Vp(self.elastic.mu, self.elastic.l, self.elastic.density)
+      print "S-wave velocity: %f" % Vs(self.elastic.mu, self.elastic.density)
+
+      # Source
+      a = 159.42
+      self.elastic.source_expression = Expression((("x[0] >= 44.5 && x[0] <= 45.5 && x[1] >= 148.5 && x[1] <= 149.5 ? (-1.0 + 2*a*pow(t - 0.3, 2))*exp(-a*pow(t - 0.3, 2)) : 0.0", "0.0"),
+                                                   ("0.0", "x[0] >= 44.5 && x[0] <= 45.5 && x[1] >= 148.5 && x[1] <= 149.5 ? (-1.0 + 2*a*pow(t - 0.3, 2))*exp(-a*pow(t - 0.3, 2)) : 0.0")), a=a, t=0)
+      self.elastic.source_function = Function(self.elastic.S)
+      self.elastic.source = self.elastic.source_expression
+
+      # Absorption
+      F = FunctionSpace(mesh, "DG", 4)
+      self.elastic.absorption_function = Function(F)
+      self.elastic.absorption = Expression("x[0] <= 20 || x[0] >= 280 || x[1] <= 20.0 ? 1000 : 0")
+
+      # Initial conditions
+      uic = Expression(('0.0', '0.0'))
+      self.elastic.u0.assign(Function(self.elastic.U).interpolate(uic))
+      sic = Expression((('0','0'),
                   ('0','0')))
-elastic.s0.assign(Function(elastic.S).interpolate(sic))
+      self.elastic.s0.assign(Function(self.elastic.S).interpolate(sic))
 
-# Start the simulation
-T = 2.5 # If you just want to run 10 time-steps, use T = elastic.dt*10.0
-elastic.run(T)
+      # Start the simulation
+      with timed_region('elastic-run'):
+         self.elastic.run(T)
+
+
+if __name__ == '__main__':
+    op2.init(log_level='WARNING')
+    from ffc.log import set_level
+    set_level('ERROR')
+
+    ExplosiveSourceLF4().explosive_source_lf4(T=2.5)
