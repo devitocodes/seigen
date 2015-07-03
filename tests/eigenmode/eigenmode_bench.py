c67e600c7099686a51f72ebf90d32bd375e263c2
Michael Lange <michael.lange@imperial.ac.uk>
2015-07-03 11:25:13 +0100

    Eigen2D: Add coffee option O3 as a parameter option

--- tests/eigenmode/eigenmode_bench.py
+++ tests/eigenmode/eigenmode_bench.py
@@ -6,6 +6,8 @@ from firedrake import *
 parameters["pyop2_options"]["profiling"] = True
 parameters["pyop2_options"]["lazy_evaluation"] = False
 
+parameters["coffee"]["O2"] = True
+
 class EigenmodeBench(Benchmark):
     warmups = 1
     repeats = 3
@@ -14,11 +16,15 @@ class EigenmodeBench(Benchmark):
     benchmark = 'Eigenmode2DLF4'
     params = [('degree', range(1, 5))]
 
-    def eigenmode2d(self, N=4, degree=1, dt=0.125, T=2.0, explicit=True):
+    def eigenmode2d(self, N=4, degree=1, dt=0.125, T=2.0,
+                    explicit=True, O3=False):
         self.series['size'] = N
         self.series['dt'] = dt
         self.series['T'] = T
         self.series['explicit'] = explicit
+        self.series['O3'] = O3
+
+        parameters["coffee"]["O3"] = O3
 
         eigen = Eigenmode2DLF4(N, degree, dt, explicit=explicit, output=False)
         eigen.eigenmode2d(T=T)
