9c12599bd995ae7435dc1792e6e356d446747f01
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2015-08-11 13:32:57 +0100

    Moar docstrings.

--- elastic_wave/helpers.py
+++ elastic_wave/helpers.py
@@ -1,9 +1,30 @@
 from math import *
 
 def Vp(mu, l, density):
-   """ Return the P-wave velocity. """
+   r""" Calculate the P-wave velocity, given by
+
+    .. math:: \sqrt{\frac{(\lambda + 2\mu)}{\rho}}
+
+   where :math:`\rho` is the density, and :math:`\lambda` and :math:`\mu` are the first and second Lamé parameters, respectively.
+
+   :param mu: The second Lamé parameter.
+   :param l: The first Lamé parameter.
+   :param density: The density.
+   :returns: The P-wave velocity.
+   :rtype: float
+   """
    return sqrt((l + 2*mu)/density)
    
 def Vs(mu, density):
-   """ Return the S-wave velocity. """
+   r""" Calculate the S-wave velocity, given by
+
+    .. math:: \sqrt{\frac{\mu}{\rho}}
+
+   where :math:`\rho` is the density, and :math:`\lambda` and :math:`\mu` is the second Lamé parameter.
+
+   :param mu: The second Lamé parameter.
+   :param density: The density.
+   :returns: The P-wave velocity.
+   :rtype: float
+   """
    return sqrt(mu/density)
