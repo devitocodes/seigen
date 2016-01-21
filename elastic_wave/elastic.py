c63362fd432ca2efd38e4a5b26f4e20b855d64cd
Michael Lange <michael.lange@imperial.ac.uk>
2016-01-21 18:05:21 +0000

    ElasticLF4: Refactor into base class with solver-specific implementations
    This commit separates implicit and explicit solver methods into separate
    implementation classes that inherit the UFL forms and I/O routines from
    and abstract base class `ElasticLF4`. Full scale wave equation solvers are
    now created with `ElasticLF4.create(mesh, dimension, degree, solver)`.
    
    This change aims at generalising `setup()` and `run()` methods and
    providing generic `create_solver()` and `solve()` routines. These create
    cached solution contexts (`LinearVariationalProblem` and `rhs(form)`)
    and provide the according `solve()` methods (`LVP.solve()` and
    `mult(rhs, inverse_mass)`).

--- elastic_wave/elastic.py
+++ elastic_wave/elastic.py
@@ -2,16 +2,44 @@
 
 from pyop2 import *
 from pyop2.profiling import timed_region
-from pyop2.utils import cached_property
 op2.init(lazy_evaluation=False)
 from firedrake import *
 from elastic_wave.helpers import log
 import mpi4py
+from abc import ABCMeta, abstractmethod
 
 
 class ElasticLF4(object):
-    r""" An elastic wave equation solver, using the finite element method for spatial discretisation,
-    and a fourth-order leap-frog time-stepping scheme. """
+    r""" An elastic wave equation solver, using the finite element
+    method for spatial discretisation, and a fourth-order leap-frog
+    time-stepping scheme.
+
+    Note that this base class only provides the UFL equations and I/O
+    methods for the model. Full scale elastic wave equation solver
+    should be created using the static method:
+    ElasticLF4.create(mesh, dimension, degree, solver)"""
+    __metaclass__ = ABCMeta
+
+    @staticmethod
+    def create(mesh, family, degree, dimension, explicit=True, output=True):
+        r""" Create an elastic wave equation solver for the given mesh
+        according to specified spatial discretisation details and
+        solver methods.
+
+        :param mesh: The underlying computational mesh of vertices and edges.
+        :param str family: Specify whether CG or DG should be used.
+        :param int degree: Use polynomial basis functions of this degree.
+        :bool explicit: If False, use PETSc to solve for the solution fields.
+                        Otherwise, explicitly invert the mass matrix and perform
+                        a matrix-vector multiplication to get the solution..
+        :param int dimension: The spatial dimension of the problem (1, 2 or 3).
+        :param bool output: If True, output the solution fields to a file.
+        :returns: None
+        """
+        if explicit:
+            return ExplicitElasticLF4(mesh, family, degree, dimension, output=output)
+        else:
+            return ImplicitElasticLF4(mesh, family, degree, dimension, output=output)
 
     def __init__(self, mesh, family, degree, dimension, explicit=True, output=True):
         r""" Initialise a new elastic wave simulation.
@@ -20,16 +48,12 @@ class ElasticLF4(object):
         :param str family: Specify whether CG or DG should be used.
         :param int degree: Use polynomial basis functions of this degree.
         :param int dimension: The spatial dimension of the problem (1, 2 or 3).
-        :param bool explicit: If False, use PETSc to solve for the solution fields.
-                              Otherwise, explicitly invert the mass matrix and perform
-                              a matrix-vector multiplication to get the solution.
         :param bool output: If True, output the solution fields to a file.
         :returns: None
         """
         with timed_region('function setup'):
             self.mesh = mesh
             self.dimension = dimension
-            self.explicit = explicit
             self.output = output
 
             self.S = TensorFunctionSpace(mesh, family, degree)
@@ -65,6 +89,10 @@ class ElasticLF4(object):
 
             self.n = FacetNormal(self.mesh)
             self.I = Identity(self.dimension)
+
+            # Inverse mass matrices for explicit methods
+            self.invmass_velocity = None
+            self.invmass_stress = None
 
         if self.output:
             with timed_region('i/o'):
@@ -102,29 +130,11 @@ class ElasticLF4(object):
         """
         self.source_function.interpolate(expression)
 
-    def assemble_inverse_mass(self):
-        r""" Compute the inverse of the consistent mass matrix for the velocity and stress equations.
-        :returns: None
-        """
-        # Inverse of the (consistent) mass matrix for the velocity equation.
-        self.inverse_mass_velocity = assemble(inner(self.w, self.u)*dx, inverse=True)
-        self.inverse_mass_velocity.assemble()
-        self.imass_velocity = self.inverse_mass_velocity.M
-        # Inverse of the (consistent) mass matrix for the stress equation.
-        self.inverse_mass_stress = assemble(inner(self.v, self.s)*dx, inverse=True)
-        self.inverse_mass_stress.assemble()
-        self.imass_stress = self.inverse_mass_stress.M
-
     @property
     def form_uh1(self):
         """ UFL for uh1 equation. """
         F = inner(self.w, self.u)*dx - self.f(self.w, self.s0, self.u0, self.n, self.absorption)
         return F
-
-    @cached_property
-    def rhs_uh1(self):
-        """ RHS for uh1 equation. """
-        return rhs(self.form_uh1)
 
     @property
     def form_stemp(self):
@@ -132,38 +142,17 @@ class ElasticLF4(object):
         F = inner(self.v, self.s)*dx - self.g(self.v, self.uh1, self.I, self.n, self.l, self.mu, self.source)
         return F
 
-    @cached_property
-    def rhs_stemp(self):
-        """ RHS for stemp equation. """
-        return rhs(self.form_stemp)
-
     @property
     def form_uh2(self):
         """ UFL for uh2 equation. """
         F = inner(self.w, self.u)*dx - self.f(self.w, self.stemp, self.u0, self.n, self.absorption)
         return F
 
-    @cached_property
-    def rhs_uh2(self):
-        """ RHS for uh2 equation. """
-        return rhs(self.form_uh2)
-
     @property
     def form_u1(self):
         """ UFL for u1 equation. """
-        if self.explicit:
-            # Note that we have multiplied through by dt here.
-            F = self.density*inner(self.w, self.u)*dx - self.density*inner(self.w, self.u0)*dx \
-                - self.dt*inner(self.w, self.uh1)*dx - ((self.dt**3)/24.0)*inner(self.w, self.uh2)*dx
-        else:
-            F = self.density*inner(self.w, (self.u - self.u0)/self.dt)*dx - inner(self.w, self.uh1)*dx \
-                - ((self.dt**2)/24.0)*inner(self.w, self.uh2)*dx
-        return F
-
-    @cached_property
-    def rhs_u1(self):
-        """ RHS for u1 equation. """
-        return rhs(self.form_u1)
+        return self.density*inner(self.w, (self.u - self.u0)/self.dt)*dx \
+            - inner(self.w, self.uh1)*dx - ((self.dt**2)/24.0)*inner(self.w, self.uh2)*dx
 
     @property
     def form_sh1(self):
@@ -171,21 +160,11 @@ class ElasticLF4(object):
         F = inner(self.v, self.s)*dx - self.g(self.v, self.u1, self.I, self.n, self.l, self.mu, self.source)
         return F
 
-    @cached_property
-    def rhs_sh1(self):
-        """ RHS for sh1 equation. """
-        return rhs(self.form_sh1)
-
     @property
     def form_utemp(self):
         """ UFL for utemp equation. """
         F = inner(self.w, self.u)*dx - self.f(self.w, self.sh1, self.u1, self.n, self.absorption)
         return F
-
-    @cached_property
-    def rhs_utemp(self):
-        """ RHS for utemp equation. """
-        return rhs(self.form_utemp)
 
     @property
     def form_sh2(self):
@@ -193,29 +172,11 @@ class ElasticLF4(object):
         F = inner(self.v, self.s)*dx - self.g(self.v, self.utemp, self.I, self.n, self.l, self.mu, self.source)
         return F
 
-    @cached_property
-    def rhs_sh2(self):
-        """ RHS for sh2 equation. """
-        return rhs(self.form_sh2)
-
     @property
     def form_s1(self):
         """ UFL for s1 equation. """
-        if self.explicit:
-            # Note that we have multiplied through by dt here.
-            F = inner(self.v, self.s)*dx - inner(self.v, self.s0)*dx \
-                - self.dt*inner(self.v, self.sh1)*dx \
-                - ((self.dt**3)/24.0)*inner(self.v, self.sh2)*dx
-        else:
-            F = inner(self.v, (self.s - self.s0)/self.dt)*dx \
-                - inner(self.v, self.sh1)*dx \
-                - ((self.dt**2)/24.0)*inner(self.v, self.sh2)*dx
-        return F
-
-    @cached_property
-    def rhs_s1(self):
-        """ RHS for s1 equation. """
-        return rhs(self.form_s1)
+        return inner(self.v, (self.s - self.s0)/self.dt)*dx \
+            - inner(self.v, self.sh1)*dx - ((self.dt**2)/24.0)*inner(self.v, self.sh2)*dx
 
     def f(self, w, s0, u0, n, absorption=None):
         """ The RHS of the velocity equation. """
@@ -234,26 +195,6 @@ class ElasticLF4(object):
             g += inner(v, source)*dx
         return g
 
-    def solve(self, rhs, matrix, result):
-        r""" Solve by assembling RHS and applying inverse mass matrix.
-        :param rhs: The RHS vector that the inverse mass matrix will be multiplied with.
-        :param matrix: The inverse mass matrix.
-        :param firedrake.Function result: The solution field.
-        :returns: None"""
-        F_a = assemble(rhs)
-        with result.dat.vec as res:
-            with F_a.dat.vec_ro as F_v:
-                matrix.handle.mult(F_v, res)
-
-    def create_solver(self, form, result):
-        r""" Create a solver object for a given form.
-        :param ufl.Form form: The weak form of the equation that needs solving.
-        :param firedrake.Function result: The field that will hold the solution.
-        :returns: A LinearVariationalSolver object associated with the problem.
-        """
-        problem = LinearVariationalProblem(lhs(form), rhs(form), result)
-        return LinearVariationalSolver(problem)
-
     def write(self, u=None, s=None):
         r""" Write the velocity and/or stress fields to file.
         :param firedrake.Function u: The velocity field.
@@ -270,30 +211,39 @@ class ElasticLF4(object):
                     # self.s_stream << s
                     pass
 
+    @abstractmethod
+    def create_solver(self, *args):
+        r""" Abstract method to generate solver context for a single form."""
+        return None
+
+    @abstractmethod
+    def solve(self, *args):
+        r""" Abstract method to solve individual form."""
+        pass
+
+    def setup(self, *args):
+        r""" Generate method-specific solver contexts for all forms."""
+        log("Creating solver contexts")
+        with timed_region('solver setup'):
+            self.ctx_uh1 = self.create_solver(self.form_uh1, self.uh1)
+            self.ctx_stemp = self.create_solver(self.form_stemp, self.stemp)
+            self.ctx_uh2 = self.create_solver(self.form_uh2, self.uh2)
+            self.ctx_u1 = self.create_solver(self.form_u1, self.u1)
+            self.ctx_sh1 = self.create_solver(self.form_sh1, self.sh1)
+            self.ctx_utemp = self.create_solver(self.form_utemp, self.utemp)
+            self.ctx_sh2 = self.create_solver(self.form_sh2, self.sh2)
+            self.ctx_s1 = self.create_solver(self.form_s1, self.s1)
+
     def run(self, T):
         """ Run the elastic wave simulation until t = T.
         :param float T: The finish time of the simulation.
         :returns: The final solution fields for velocity and stress.
         """
-        self.write(self.u1, self.s1)  # Write out the initial condition.
+        # Write out the initial condition.
+        self.write(self.u1, self.s1)
 
-        if self.explicit:
-            log("Generating inverse mass matrix")
-            # Pre-assemble the inverse mass matrices, which should stay
-            # constant throughout the simulation (assuming no mesh adaptivity).
-            with timed_region('inverse mass matrix'):
-                self.assemble_inverse_mass()
-        else:
-            log("Creating solver contexts")
-            with timed_region('solver setup'):
-                solver_uh1 = self.create_solver(self.form_uh1, self.uh1)
-                solver_stemp = self.create_solver(self.form_stemp, self.stemp)
-                solver_uh2 = self.create_solver(self.form_uh2, self.uh2)
-                solver_u1 = self.create_solver(self.form_u1, self.u1)
-                solver_sh1 = self.create_solver(self.form_sh1, self.sh1)
-                solver_utemp = self.create_solver(self.form_utemp, self.utemp)
-                solver_sh2 = self.create_solver(self.form_sh2, self.sh2)
-                solver_s1 = self.create_solver(self.form_s1, self.s1)
+        # Call solver-specific setup
+        self.setup()
 
         with timed_region('timestepping'):
             t = self.dt
@@ -307,33 +257,19 @@ class ElasticLF4(object):
                         self.source = self.source_expression
 
                 # Solve for the velocity vector field.
-                if self.explicit:
-                    with timed_region('velocity solve'):
-                        self.solve(self.rhs_uh1, self.imass_velocity, self.uh1)
-                        self.solve(self.rhs_stemp, self.imass_stress, self.stemp)
-                        self.solve(self.rhs_uh2, self.imass_velocity, self.uh2)
-                        self.solve(self.rhs_u1, self.imass_velocity, self.u1)
-                else:
-                    with timed_region('velocity solve'):
-                        solver_uh1.solve()
-                        solver_stemp.solve()
-                        solver_uh2.solve()
-                        solver_u1.solve()
+                with timed_region('velocity solve'):
+                    self.solve(self.ctx_uh1, self.invmass_velocity, self.uh1)
+                    self.solve(self.ctx_stemp, self.invmass_stress, self.stemp)
+                    self.solve(self.ctx_uh2, self.invmass_velocity, self.uh2)
+                    self.solve(self.ctx_u1, self.invmass_velocity, self.u1)
                 self.u0.assign(self.u1)
 
                 # Solve for the stress tensor field.
-                if self.explicit:
-                    with timed_region('stress solve'):
-                        self.solve(self.rhs_sh1, self.imass_stress, self.sh1)
-                        self.solve(self.rhs_utemp, self.imass_velocity, self.utemp)
-                        self.solve(self.rhs_sh2, self.imass_stress, self.sh2)
-                        self.solve(self.rhs_s1, self.imass_stress, self.s1)
-                else:
-                    with timed_region('stress solve'):
-                        solver_sh1.solve()
-                        solver_utemp.solve()
-                        solver_sh2.solve()
-                        solver_s1.solve()
+                with timed_region('stress solve'):
+                    self.solve(self.ctx_sh1, self.invmass_stress, self.sh1)
+                    self.solve(self.ctx_utemp, self.invmass_velocity, self.utemp)
+                    self.solve(self.ctx_sh2, self.invmass_stress, self.sh2)
+                    self.solve(self.ctx_s1, self.invmass_stress, self.s1)
                 self.s0.assign(self.s1)
 
                 # Write out the new fields
@@ -343,3 +279,73 @@ class ElasticLF4(object):
                 t += self.dt
 
         return self.u1, self.s1
+
+
+class ImplicitElasticLF4(ElasticLF4):
+    r""" Elastic equation solver that implicitly solves individual UFL
+    forms by wrapping them as firedrake.LinearVariatonalProblem().
+    """
+    def create_solver(self, form, result):
+        r""" Create a solver object for a given form.
+        :param ufl.Form form: The weak form of the equation that needs solving.
+        :param firedrake.Function result: The field that will hold the solution.
+        :returns: A LinearVariationalSolver object associated with the problem.
+        """
+        problem = LinearVariationalProblem(lhs(form), rhs(form), result)
+        return LinearVariationalSolver(problem)
+
+    def solve(self, solver, *args):
+        solver.solve()
+
+
+class ExplicitElasticLF4(ElasticLF4):
+    r""" Elastic equation solver that explicitly solves individual UFL
+    forms by assembling RHS vectors and multiplying them with the
+    according global inverse mass matrix.
+    """
+    @property
+    def form_u1(self):
+        """ UFL for u1 equation. """
+        # Note that we have multiplied through by dt here.
+        return self.density*inner(self.w, self.u)*dx - self.density*inner(self.w, self.u0)*dx \
+            - self.dt*inner(self.w, self.uh1)*dx - ((self.dt**3)/24.0)*inner(self.w, self.uh2)*dx
+
+    @property
+    def form_s1(self):
+        """ UFL for s1 equation. """
+        # Note that we have multiplied through by dt here.
+        return inner(self.v, self.s)*dx - inner(self.v, self.s0)*dx \
+            - self.dt*inner(self.v, self.sh1)*dx - ((self.dt**3)/24.0)*inner(self.v, self.sh2)*dx
+
+    def create_solver(self, form, *args):
+        r""" Solution context for explicit methods is the compiled RHS kernel object."""
+        return rhs(form)
+
+    def solve(self, rhs, matrix, result):
+        r""" Solve by assembling into RHS vector and applying inverse mass matrix.
+        :param rhs: The RHS vector that the inverse mass matrix will be multiplied with.
+        :param matrix: The inverse mass matrix.
+        :param firedrake.Function result: The solution field.
+        :returns: None"""
+        F_a = assemble(rhs)
+        with result.dat.vec as res:
+            with F_a.dat.vec_ro as F_v:
+                matrix.handle.mult(F_v, res)
+
+    def setup(self):
+        r""" Pre-assembles the inverse of the consistent mass matrix for the
+        velocity and stress equations.
+        :returns: None
+        """
+        log("Generating inverse mass matrices")
+        # Inverse of the (consistent) mass matrix for the velocity equation.
+        self.inverse_mass_velocity = assemble(inner(self.w, self.u)*dx, inverse=True)
+        self.inverse_mass_velocity.assemble()
+        self.invmass_velocity = self.inverse_mass_velocity.M
+        # Inverse of the (consistent) mass matrix for the stress equation.
+        self.inverse_mass_stress = assemble(inner(self.v, self.s)*dx, inverse=True)
+        self.inverse_mass_stress.assemble()
+        self.invmass_stress = self.inverse_mass_stress.M
+
+        # Setup RHS assembly objects
+        super(ExplicitElasticLF4, self).setup()
