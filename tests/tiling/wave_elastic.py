cf02bf28ce8e886e3f15a8074f7918411ff21315
Fabio Luporini <fl1612@imperial.ac.uk>
2016-06-06 14:17:19 +0100

    cx1: Mirror explicit modes

--- tests/tiling/wave_elastic.py
+++ tests/tiling/wave_elastic.py
@@ -122,9 +122,10 @@ class ElasticLF4(object):
                         os.makedirs(base)
                     sub_dirs = [d for d in os.listdir(base)
                                 if os.path.isdir(os.path.join(base, d))]
-                    sub_dir = "%d_part%s_tile%s" % (len(sub_dirs),
-                                                    self.tiling_size if self.tiling_uf else 0,
-                                                    self.tiling_part if self.tiling_uf else 'None')
+                    sub_dir = "%d_em%d_part%s_tile%s" % (len(sub_dirs),
+                                                         self.tiling_explicit_id if self.tiling_explicit_id else 0,
+                                                         self.tiling_size if self.tiling_uf else 0,
+                                                         self.tiling_part if self.tiling_uf else 'None')
                     base = os.path.join(base, sub_dir)
                     os.makedirs(base)
                 op2.MPI.comm.barrier()
@@ -403,6 +404,19 @@ class ElasticLF4(object):
                      arg0,
                      arg1)
 
+    def footprint(self):
+        """Track memory footprint of all loops in the trace."""
+        from pyop2.base import ParLoop
+        if op2.MPI.comm.rank == 0:
+            loops = [l for l in _trace._trace if isinstance(l, ParLoop)]
+            for i, l in enumerate(loops):
+                l_size = 0
+                for a in l.args:
+                    dat_size = a.data.nbytes
+                    map_size = a.map.values_with_halo.nbytes if a.map else 0
+                    l_size += (dat_size + map_size)/1000
+                print "Loop", i, "(kernel =", l.kernel._name, ") :: ", l_size, "KB"
+
     def write(self, u=None, s=None, output=True):
         r""" Write the velocity and/or stress fields to file.
         :param firedrake.Function u: The velocity field.
@@ -470,6 +484,10 @@ class ElasticLF4(object):
                 self.solve(self.rhs_utemp, self.velocity_mass_asdat, self.utemp)
                 self.solve(self.rhs_sh2, self.stress_mass_asdat, self.sh2)
                 self.solve(self.rhs_s1, self.stress_mass_asdat, self.s1)
+
+                # Track memory footprint of each loop in the chain
+                #if timestep == 0:
+                #    self.footprint()
 
             self.u0.assign(self.u1)
             self.s0.assign(self.s1)
@@ -539,20 +557,19 @@ class FusionModes():
 
     # (num_solves, [(first_loop_index, last_loop_index, tile_size_multiplier)])
     modes = {
-        1: (1, [(1, 2, 4)]),
-        2: (1, [(1, 2, 4), (8, 9, 4), (17, 18, 4)]),
-        3: (1, [(1, 3, 4), (8, 10, 4), (17, 19, 4)]),
-        4: (1, [(1, 2, 4), (4, 6, 4), (8, 9, 4), (10, 12, 2),
+        1: (1, [(1, 3, 4), (8, 10, 4), (17, 19, 4)]),
+        2: (1, [(1, 2, 4), (4, 6, 4), (8, 9, 4), (10, 12, 2),
                 (13, 15, 4), (17, 18, 4), (20, 22, 4), (23, 25, 1)]),
-        5: (1, [(1, 3, 4), (4, 7, 4), (8, 12, 2), (13, 16, 4), (17, 19, 4), (20, 25, 1)]),
-        6: (4, [(0, 12, 1), (13, 25, 1)]),
-        7: (8, [(0, 25, 1)])
+        3: (1, [(1, 3, 4), (4, 7, 4), (8, 12, 2), (13, 16, 4), (17, 19, 4), (20, 25, 1)]),
+        4: (2, [(1, 7, 1), (8, 16, 1),  (17, 25, 1)]),
+        5: (4, [(0, 12, 1), (13, 25, 1)]),
+        6: (8, [(0, 25, 1)])
     }
 
     @staticmethod
-    def get(mode, tile_size):
+    def get(mode, part_mode, tile_size):
         num_solves, mode = FusionModes.modes[mode]
-        mode = [(i, j, tile_size*k) for i, j, k in mode]
+        mode = [(i, j, tile_size*(k if part_mode == 'chunk' else 1)) for i, j, k in mode]
         return num_solves, mode
 
 
@@ -575,7 +592,7 @@ class ExplosiveSourceLF4():
         split_mode = tiling['split_mode']
         explicit_mode = tiling['explicit_mode']
         if explicit_mode:
-            tiling['explicit_mode'] = FusionModes.get(explicit_mode, tile_size)[1]
+            tiling['explicit_mode'] = FusionModes.get(explicit_mode, part_mode, tile_size)[1]
 
         with timed_region('mesh generation'):
             # Get a mesh ...
@@ -588,7 +605,7 @@ class ExplosiveSourceLF4():
             else:
                 num_solves = ElasticLF4.num_solves
                 if explicit_mode:
-                    num_solves = FusionModes.get(explicit_mode, tile_size)[0]
+                    num_solves = FusionModes.get(explicit_mode, part_mode, tile_size)[0]
                 elif split_mode > 0 and split_mode < num_solves:
                     num_solves = split_mode
                 s_depth = calculate_sdepth(num_solves, num_unroll, extra_halo)
@@ -667,14 +684,6 @@ class ExplosiveSourceLF4():
                     poly_order=poly_order,
                     domain=os.path.splitext(os.path.basename(mesh.name))[0])
 
-        import sys
-        for i in range(op2.MPI.comm.size):
-            if op2.MPI.comm.rank == i:
-                print "PRINTING SUMMARY FOR RANK", i
-                summary()
-                sys.stdout.flush()
-            op2.MPI.comm.barrier()
-
         return u1, s1
 
 
@@ -704,6 +713,7 @@ if __name__ == '__main__':
         'coloring': args.coloring,
         'extra_halo': args.extra_halo,
         'split_mode': args.split_mode,
+        'explicit_mode_id': args.explicit_mode,
         'explicit_mode': args.explicit_mode,
         'use_glb_maps': eval(args.glb_maps) if args.glb_maps else False,
         'use_prefetch': eval(args.prefetch) if args.prefetch else False,
