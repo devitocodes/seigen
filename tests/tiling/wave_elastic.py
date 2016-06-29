bc477dfdc562528a91516997e12fa686bd8ce0ec
Fabio Luporini <f.luporini12@imperial.ac.uk>
2016-06-29 09:08:45 +0100

    tiling: Synchronize absorption poly order

--- tests/tiling/wave_elastic.py
+++ tests/tiling/wave_elastic.py
@@ -632,7 +632,7 @@ class ExplosiveSourceLF4():
         self.elastic.source = self.elastic.source_expression
 
         # Absorption
-        F = FunctionSpace(mesh, "DG", 4, name='F')
+        F = FunctionSpace(mesh, "DG", poly_order, name='F')
         self.elastic.absorption_function = Function(F)
         self.elastic.absorption = Expression("x[0] <= 20 || x[0] >= %f || x[1] <= 20.0 ? 1000 : 0" % (Lx - 20,))
 
