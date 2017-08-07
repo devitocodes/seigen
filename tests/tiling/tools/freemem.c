fd6e4c455388e3223d2a8a036ed05969164e7225
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-08-07 11:59:48 +0200

    Add docstring to freemem.c

--- tests/tiling/tools/freemem.c
+++ tests/tiling/tools/freemem.c
@@ -1,3 +1,10 @@
+/* This file is not part of the tiling example -- it is only here to work
+ * around an OS bug on the Imperial College Cluster CX2, to make sure that
+ * processes allocate memory on the closest NUMA domain. In case of MPI
+ * execution, it is recommended to run this code through `runfreemem.py`.
+ * The launcher scripts automatically execute this prior to execution.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
